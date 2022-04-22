ffbecf7e37979c361a7afbe13d20f12469d8e8ac
Denis Kotlica <neosiae@gmail.com>
2022-04-22 14:00:05 +0200

    feat: Add github

--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -5,6 +5,7 @@ const Links = () => {
     <div className="links">
       <ul>
         <li><a href="/posts">blog</a></li>
+        <li><a href="https://github.com/neosiae" target="_blank">github</a></li>
         <li><a href="https://soundcloud.com/neosiae" target="_blank">soundcloud</a></li>
       </ul>
     </div>
